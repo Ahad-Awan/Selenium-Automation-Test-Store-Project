a1d2007b15d75991f23238d03b2789fc51f957f6
Ahad-Awan <muhammadahad203813@gmail.com>
2025-08-18 15:48:34 +0500

    .

--- utils/driver.js
+++ utils/driver.js
@@ -1,8 +1,21 @@
 const { Builder } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 
 async function createDriver() {
-  let driver = await new Builder().forBrowser("chrome").build();
-  await driver.manage().window().maximize();
+  let options = new chrome.Options();
+
+  // Headless mode (Jenkins/CI ke liye zaroori)
+  options.addArguments("--headless");
+  options.addArguments("--no-sandbox");
+  options.addArguments("--disable-dev-shm-usage");
+  options.addArguments("--disable-gpu");
+  options.addArguments("--window-size=1920,1080"); // maximize replacement
+
+  let driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(options)
+    .build();
+
   return driver;
 }
 
