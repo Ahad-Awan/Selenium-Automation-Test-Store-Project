f35e6252ca2cb15d35952a7fdbca4169bf12976c
Ahad-Awan <muhammadahad203813@gmail.com>
2025-08-20 12:39:04 +0500

    Update driver.js

--- utils/driver.js
+++ utils/driver.js
@@ -4,12 +4,11 @@ const chrome = require("selenium-webdriver/chrome");
 async function createDriver() {
   let options = new chrome.Options();
 
-  // Jenkins / CI ke liye recommended flags
-  options.addArguments("--headless=new"); // headless mode
-  options.addArguments("--disable-gpu"); // GPU disable (headless stability)
-  options.addArguments("--no-sandbox"); // sandbox issue avoid
-  options.addArguments("--disable-dev-shm-usage"); // shared memory issue avoid
-  options.addArguments("--window-size=1920,1080"); // full HD screen size
+  options.addArguments("--headless=new");
+  options.addArguments("--disable-gpu");
+  options.addArguments("--no-sandbox");
+  options.addArguments("--disable-dev-shm-usage");
+  options.addArguments("--window-size=1920,1080");
 
   let driver = await new Builder()
     .forBrowser("chrome")
